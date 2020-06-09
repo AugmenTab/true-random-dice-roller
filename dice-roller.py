be20b258f57652d8bf48ad3f57f01e2dc6448890
AugmenTab <tbaum92@live.com>
2020-06-08 19:01:56 -0600

    Added request info for API.

--- dice-roller.py
+++ dice-roller.py
@@ -1,1 +1,17 @@
 #! python3
+
+import requests
+
+reqData = {
+    "jsonrpc": "2.0",
+    "method": "generateIntegers",
+    "params": {
+        "apiKey": "00000000-0000-0000-0000-000000000000",
+        "n": 0,
+        "min": 1,
+        "max": 0,
+        "replacement": True,
+        "base": 10
+    },
+    "id": 18730
+}
